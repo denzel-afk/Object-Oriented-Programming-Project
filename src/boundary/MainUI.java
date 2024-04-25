0ee3d920fac4d429e8c40d9500f778db976ba965
denzel-afk <denzeledanbanget@gmail.com>
2024-04-26 04:13:23 +0800

    feature: adding boundaries

---
+++ src/boundary/MainUI.java
@@ -0,0 +1,51 @@
+package boundary;
+
+import java.util.*;
+
+import entity.Company;
+import exception.AddStaffException;
+import exception.BranchExistsException;
+import exception.BranchNotExistException;
+import exception.PromotionException;
+import exception.StaffExistException;
+import exception.WrongPasswordException;
+
+public class MainUI {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void MainPageUI(){
+		
+		System.out.println("Welcome to 20-02 Fast Food Chain.");
+	    
+	    int choice;
+	    
+	    do {
+	      choice = roleChoice();
+	      
+	      switch(choice) {
+	        case 1:
+	          CustomerMainPage.CustomerMainPageUI();
+	          break;
+	        case 2:
+	          EmployeeLogin.employeeLoginUI();
+	          break;
+	        case 3:
+	          System.out.println("Bye!");
+	          break;
+	        default:
+	          System.out.println("Try again");
+	          break;
+	      }
+	    } while (choice!=3);
+	}
+	
+	public static int roleChoice() {
+		
+		System.out.println("Enter your role");
+		System.out.println("1: Customer\n2: Staff");
+		int choice = sc.nextInt();
+		
+		return choice;	
+	}
+}
---
+++ src/boundary/UserChangePassword.java
@@ -0,0 +1,62 @@
+package boundary;
+
+public class UserChangePassword {
+
+}
+package boundary;
+
+import java.util.Scanner;
+
+import entity.Admin;
+import entity.Company;
+import entity.Staff;
+import entity.User;
+import exception.AddStaffException;
+import exception.BranchExistsException;
+import exception.BranchNotExistException;
+import exception.PromotionException;
+import exception.StaffExistException;
+import exception.WrongPasswordException;
+import control.AccountController;
+import control.PasswordController;
+
+public class UserChangePassword {
+	
+	static Scanner sc = new Scanner(System.in);
+
+	@SuppressWarnings("incomplete-switch")
+	public static void changePasswordUI(User user){
+		
+		System.out.println("Enter your old password");
+		String oldPassword = sc.next();
+		
+		System.out.println("Enter your new password");
+		String newPassword = sc.next();
+		
+		try {
+			PasswordController.changePassword(user, oldPassword, newPassword);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Returning to previous page...");
+			return;
+		}
+
+		
+		if (user instanceof Admin)
+			AdminMainPage.AdminUI(user);
+		
+		if (user instanceof Staff) {
+			Staff staffUser = (Staff) user; 
+			
+			switch(staffUser.getRole()) { //need to add getRole in StaffController class?
+				case STAFF:
+					StaffMainPage.StaffUI(staffUser);
+					break;
+				case MANAGER:
+					ManagerMainPage.ManagerUI(staffUser);
+			}
+		}
+		
+	}
+
+}
