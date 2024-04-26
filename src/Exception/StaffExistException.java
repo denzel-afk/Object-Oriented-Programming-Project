9b2a60e331dee9f02e028f24b6496722e36150c7
Denzel Elden Wijaya <137489821+denzel-afk@users.noreply.github.com>
2024-04-26 18:19:41 +0800

    StaffExistException.java

--- src/Exception/StaffExistException.java
+++ src/Exception/StaffExistException.java
@@ -10,10 +10,17 @@ package exception;
  * @since 2021-04-26
  */
 public class StaffExistException extends Exception {
+    /**
+     * The serial version UID.
+     */
     public StaffExistException() {
         super("Staff's ID already existed");
     }
-
+    /**
+     * The constructor for the StaffExistException class.
+     * 
+     * @param message The error message.
+     */
     public StaffExistException(String message) {
         super(message);
     }
