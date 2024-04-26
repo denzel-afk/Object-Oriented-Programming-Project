b4a9e9ee2f32e0b000649c8df58a74b3b3305fde
Denzel Elden Wijaya <137489821+denzel-afk@users.noreply.github.com>
2024-04-26 18:20:32 +0800

    PromotionException.java

--- src/Exception/PromotionException.java
+++ src/Exception/PromotionException.java
@@ -10,10 +10,17 @@ package exception;
  * @since 2021-04-26
  */
 public class PromotionException extends Exception {
+    /**
+     * The serial version UID.
+     */
     public PromotionException() {
         super("Unable to promote staff");
     }
-
+    /**
+     * The constructor for the PromotionException class.
+     * 
+     * @param message The error message.
+     */
     public PromotionException(String message) {
         super(message);
     }
