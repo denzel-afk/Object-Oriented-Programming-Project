133707d4390fbcae6f347730920e194d4db08ca8
Denzel Elden Wijaya <137489821+denzel-afk@users.noreply.github.com>
2024-04-26 18:08:35 +0800

    OrderException.java

--- src/Exception/OrderException.java
+++ src/Exception/OrderException.java
@@ -1,5 +1,13 @@
 package exception;
-
+/**
+ * A custom exception class for handling errors related to undetermined orders.
+ * @author Denzel Elden Wijaya
+ * @author Federrico Hansen Budianto
+ * @author Melisa Lee
+ * @author Rivaldo Billy Sebastian
+ * @version 1.0
+ * @since 2021-04-26
+ */
 public class OrderException extends Exception {
 	public OrderException() {
 		super("Order is undetermined");
