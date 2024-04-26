eb9bb4fe50fc24ef2038f2551741de7fc764e65a
denzel-afk <denzeledanbanget@gmail.com>
2024-04-26 22:41:05 +0800

    featureL+: another javadoc

--- src/package-search-index.js
+++ src/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"boundary"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"exception"}];updateSearchResults();
