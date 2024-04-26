622cbf39ec421c69ed708b3224fb4719ecc452f3
Denzel Elden Wijaya <137489821+denzel-afk@users.noreply.github.com>
2024-04-26 18:10:11 +0800

    PromotionException.java

--- src/Exception/PromotionException.java
+++ src/Exception/PromotionException.java
@@ -1,5 +1,14 @@
 package exception;
-
+/**
+ * A custom exception class for handling errors related to staff promotion.
+ * 
+ * @author Denzel Elden Wijaya
+ * @author Federrico Hansen Budianto
+ * @author Melisa Lee
+ * @author Rivaldo Billy Sebastian
+ * @version 1.0
+ * @since 2021-04-26
+ */
 public class PromotionException extends Exception {
     public PromotionException() {
         super("Unable to promote staff");
