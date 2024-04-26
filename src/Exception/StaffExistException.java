2eb5f6d24fc468c5a3d9d7bdb6fb80f35ac6df9a
Denzel Elden Wijaya <137489821+denzel-afk@users.noreply.github.com>
2024-04-26 18:11:50 +0800

    StaffExistException.java

--- src/Exception/StaffExistException.java
+++ src/Exception/StaffExistException.java
@@ -1,5 +1,14 @@
 package exception;
-
+/**
+ * A custom exception class for handling errors related to staff existence.
+ * 
+ * @author Denzel Elden Wijaya
+ * @author Federrico Hansen Budianto
+ * @author Melisa Lee
+ * @author Rivaldo Billy Sebastian
+ * @version 1.0
+ * @since 2021-04-26
+ */
 public class StaffExistException extends Exception {
     public StaffExistException() {
         super("Staff's ID already existed");
