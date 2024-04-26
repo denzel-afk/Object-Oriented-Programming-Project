f0b58b1a081cb5b8fe5be0aed7c6957d420ad269
Denzel Elden Wijaya <137489821+denzel-afk@users.noreply.github.com>
2024-04-26 18:15:28 +0800

    StaffNoExistenceException.java

--- src/Exception/StaffNoExistenceException.java
+++ src/Exception/StaffNoExistenceException.java
@@ -1,5 +1,15 @@
 package exception;
-
+/**
+ * A custom exception class for handling errors related to non-existing staff
+ * login IDs.
+ * 
+ * @author Denzel Elden Wijaya
+ * @author Federrico Hansen Budianto
+ * @author Melisa Lee
+ * @author Rivaldo Billy Sebastian
+ * @version 1.0
+ * @since 2021-04-26
+ */
 public class StaffNoExistenceException extends Exception{
 	public StaffNoExistenceException() {
 		super("Staff login ID does not exist");
