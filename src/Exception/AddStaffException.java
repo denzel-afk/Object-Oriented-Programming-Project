b4e886666250fa9d73739afd60090c526d55ad1d
denzel-afk <denzeledanbanget@gmail.com>
2024-04-26 17:45:38 +0800

    feature: change addStaffException

--- src/Exception/AddStaffException.java
+++ src/Exception/AddStaffException.java
@@ -1,14 +1,18 @@
+package exception;
+
 /**
  * A custom exception class for handling errors related to adding staff.
- @author Denzel Elden Wijaya
- @version 1.0
- @since 2024-04-26
+ * 
+ * @author Denzel Elden Wijaya
+ * @version 1.0
+ * @since 2024-04-26
  */
 public class AddStaffException extends Exception {
 
     /**
-     * Constructs a new AddStaffException with a default message, where this exception will be called
-     when a staff can not be added.
+     * Constructs a new AddStaffException with a default message, where this
+     * exception will be called
+     * when a staff can not be added.
      */
     public AddStaffException() {
         super("Staff cannot be added");
@@ -17,10 +21,10 @@ public class AddStaffException extends Exception {
     /**
      * Constructs a new AddStaffException with the specified detail message.
      *
-     * @param message the detail message (which is saved for later retrieval by the getMessage() method)
+     * @param message the detail message (which is saved for later retrieval by the
+     *                getMessage() method)
      */
     public AddStaffException(String message) {
         super(message);
     }
 }
-
